326319d9f35a1d4fe0c6cf7673dc881767e67def
Yeneviel Roberts <yenevielroberts@gmail.com>
2025-10-18 13:53:16 +0200

    header estilo, home, forms y lista

--- public/js/header.js
+++ public/js/header.js
@@ -4,10 +4,13 @@ const nav = document.createElement('nav');
 
 nav.innerHTML = `
 
-    <a href="/">Home</a>
-    <a href="/login">Log in</a>
+    <a href="/protected">Home</a>
+    <a href="/">Log in</a>
     <a href="/signup">Sign up</a>
 `;
 
-header.appendChild(nav)
+header.style="display:flex; flex-direction:row; background-color:#9D174D; width:100%; height:70px; justify-content: right; align-items:centet;"
 
+nav.style=" margin:5px; padding:5px;"
+
+header.appendChild(nav)
